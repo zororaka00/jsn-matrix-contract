78e79753dedb2645681a244b76a03fde5a5b105e
zororaka00 <rakawidhiantoro@gmail.com>
2022-11-21 00:12:03 +0700

    .

--- scripts/deploy_production.ts
+++ scripts/deploy_production.ts
@@ -1,22 +1,35 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
   const addressBUSD = "0x9C9e5fD8bbc25984B178FdCE6117Defa39d2db39";
 
   let instance_shareowner = [];
   const ShareOwner = await ethers.getContractFactory("ShareOwner");
   instance_shareowner[0] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[1] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[2] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[3] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[4] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[5] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[6] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[7] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[8] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[9] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[10] = await ShareOwner.deploy();
+  await delay(5000);
   instance_shareowner[11] = await ShareOwner.deploy();
+  await delay(5000);
   let address_shareowner = await instance_shareowner.map((d: any) => d.address);
   console.log(`Address Share Owner Contract: ${address_shareowner}`);
 
