a6e1de0dd77553ff432045253c9246ab33c0d9eb
zororaka00 <rakawidhiantoro@gmail.com>
2022-12-26 17:09:22 +0700

    .

--- contracts/SwapJSN.sol
+++ contracts/SwapJSN.sol
@@ -5,10 +5,9 @@ import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/InterfaceWMATIC.sol";
 
-contract SwapJSN is Context, ReentrancyGuard {
+contract SwapJSN is Context {
     ISwapRouter public immutable swapRouter;
     IWMATIC public WMATIC;
 
@@ -19,19 +18,73 @@ contract SwapJSN is Context, ReentrancyGuard {
         0xa8bf3aC4f567384F2f44B4E7C6d11b7664749f35 // Owner 2
     ];
 
-    constructor(address _addressWMATIC, address _swapRouter) {
-        WMATIC = IWMATIC(_addressWMATIC);
+    constructor(address _WMATIC, address _swapRouter) {
+        WMATIC = IWMATIC(_WMATIC);
         swapRouter = ISwapRouter(_swapRouter);
     }
-    
-    function swap(address who, address _sendTo, address _tokenIn, address _tokenOut, uint256 _amountIn, uint24 _feeTier) internal returns (uint256 amountOut) {
 
-        IERC20 tokenIn = IERC20(_tokenIn);
+    receive() external payable {}
 
-        if (_tokenIn != address(WMATIC)) {
-            tokenIn.transferFrom(who, address(this), _amountIn);
+    function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint24 _feeTier) external payable returns (uint256) {
+        address who = _msgSender();
+
+        address addressNull = address(0);
+        uint256 valueCoin = _tokenIn == addressNull ? msg.value : _amountIn;
+        uint256 amountIn = shareFee(_tokenIn, who, valueCoin);
+
+        address tokenIn = _tokenIn;
+        if (_tokenIn == addressNull) {
+            tokenIn = address(WMATIC);
+            WMATIC.deposit{ value: amountIn }();
         }
-        tokenIn.approve(address(swapRouter), _amountIn);
+        address tokenOut = _tokenOut;
+        address sendTo = who;
+        if (_tokenOut == addressNull) {
+            tokenOut = address(WMATIC);
+            sendTo = address(this);
+        }
+        
+        uint256 amountOut = swapInternal(who, sendTo, tokenIn, tokenOut, amountIn, _feeTier);
+        if (_tokenOut == addressNull) {
+            WMATIC.withdraw(amountOut);
+            Address.sendValue(payable(who), amountOut);
+        }
+        return amountOut;
+    }
+
+    function swapTo(address _tokenIn, address _tokenOut, address _sendTo, uint256 _amountIn, uint24 _feeTier) external payable returns (uint256) {
+        address who = _msgSender();
+
+        address addressNull = address(0);
+        uint256 valueCoin = _tokenIn == addressNull ? msg.value : _amountIn;
+        uint256 amountIn = shareFee(_tokenIn, who, valueCoin);
+
+        address tokenIn = _tokenIn;
+        if (_tokenIn == addressNull) {
+            tokenIn = address(WMATIC);
+            WMATIC.deposit{ value: amountIn }();
+        }
+        address tokenOut = _tokenOut;
+        address sendTo = _sendTo;
+        if (_tokenOut == addressNull) {
+            tokenOut = address(WMATIC);
+            sendTo = address(this);
+        }
+        
+        uint256 amountOut = swapInternal(who, sendTo, tokenIn, tokenOut, amountIn, _feeTier);
+        if (_tokenOut == addressNull) {
+            WMATIC.withdraw(amountOut);
+            Address.sendValue(payable(_sendTo), amountOut);
+        }
+        return amountOut;
+    }
+    
+    function swapInternal(address who, address _sendTo, address _tokenIn, address _tokenOut, uint256 _amountIn, uint24 _feeTier) internal returns (uint256) {
+        IERC20 token = IERC20(_tokenIn);
+        if (_tokenIn != address(WMATIC)) {
+            token.transferFrom(who, address(this), _amountIn);
+        }
+        token.approve(address(swapRouter), _amountIn);
         
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
@@ -45,28 +98,23 @@ contract SwapJSN is Context, ReentrancyGuard {
                 sqrtPriceLimitX96: 0
             });
             
-        amountOut = swapRouter.exactInputSingle(params);
-    }
-
-    function shareFeeETH(uint256 _amount) internal returns (uint256) {
-        uint256 fee = _amount * feeSwap / 1e6;
-
-        uint256 shareOwner1 = fee * shareOwner / 100;
-        uint256 shareOwner2 = fee - shareOwner1;
-        Address.sendValue(payable(payeesOwner[0]), shareOwner1);
-        Address.sendValue(payable(payeesOwner[1]), shareOwner2);
-
-        return _amount - fee;
+        uint256 amountOut = swapRouter.exactInputSingle(params);
+        return amountOut;
     }
 
     function shareFee(address _token, address who, uint256 _amount) internal returns (uint256) {
         uint256 fee = _amount * feeSwap / 1e6;
         
-        IERC20 token = IERC20(_token);
         uint256 shareOwner1 = fee * shareOwner / 100;
         uint256 shareOwner2 = fee - shareOwner1;
-        token.transferFrom(who, payeesOwner[0], shareOwner1);
-        token.transferFrom(who, payeesOwner[1], shareOwner2);
+        if (_token == address(0)) {
+            Address.sendValue(payable(payeesOwner[0]), shareOwner1);
+            Address.sendValue(payable(payeesOwner[1]), shareOwner2);
+        } else {
+            IERC20 token = IERC20(_token);
+            token.transferFrom(who, payeesOwner[0], shareOwner1);
+            token.transferFrom(who, payeesOwner[1], shareOwner2);
+        }
 
         return _amount - fee;
     }
