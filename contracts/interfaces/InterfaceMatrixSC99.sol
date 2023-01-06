27720d6f764a9f2df8d75c8884e90b2f24a8b401
zororaka00 <rakawidhiantoro@gmail.com>
2023-01-06 22:12:34 +0700

    .

--- contracts/interfaces/InterfaceMatrixSC99.sol
+++ contracts/interfaces/InterfaceMatrixSC99.sol
@@ -2,5 +2,8 @@ pragma solidity ^0.8.0;
 
 interface InterfaceMatrixSC99 {
     function lineMatrix(uint256) external view returns(uint256);
-    function receivedBUSD(address) external view returns(uint256);
+    function receivedBUSD(uint256) external view returns(uint256);
+    function allTokenIds(address) external view returns (uint256[] memory);
+    function allInfo(address) external view returns (uint256[] memory, uint256[] memory);
+    function totalReceivedBUSD(address) external view returns(uint256);
 }
